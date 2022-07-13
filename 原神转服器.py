5961f6540e36602539fca44b6d5eaf8ade9c617b
惠惠 <108872679+gtgor@users.noreply.github.com>
2022-07-14 07:41:28 +0800

    Add files via upload

--- 原神转服器.py
+++ 原神转服器.py
@@ -1,19 +1,12 @@
 import shutil
 import tkinter
 import os
-import winreg
+
 import requests
 import win32com.client
+import winreg
 
 字体大小=10
-
-def 检查是否有新版本():
-    当前版本=open("data/当前转服器版本","r").read()
-    最新版本=requests.get(url="http://8.142.28.115/版本").text
-    if 当前版本 != 最新版本:
-        return True
-    else:
-        return False
 
 def 提示(title,data):
     错误提示=tkinter.Tk()
@@ -38,9 +31,7 @@ def 获取快捷方式指向的位置(path):
     shortcut = shell.CreateShortCut(path)
     return shortcut.Targetpath
 
-def 获取桌面原神快捷方式():
-    path=获取桌面路径()+r"\原神.lnk"
-    return path
+
 
 def 保存路径(path):
     #对用户输入的路径进行修改
@@ -57,46 +48,64 @@ def 保存路径按钮命令():
     保存路径(path)
 
 def 自动获取路径按钮命令():
-    path = 获取快捷方式指向的位置(获取桌面原神快捷方式())
-    path=path.replace(r"\launcher.exe",'')
-    保存路径(path)
-    提示("可爱の惠惠の提示","路径保存成功QAQ（快夸我）")
+    路径=False
+    磁盘=["C","D","E","F","G",'H',"A","B"]
+    for a in 磁盘:
+        try:
+            路径=search(a+r":\\","Genshin Impact Game")
+            if 路径==-1:
+                pass
+            else:
+                保存路径(路径.replace(r"\\",r"\惠惠惠惠惠惠").replace(r"惠惠惠惠惠惠",r""))
+
+                提示("可爱の惠惠の提示","路径保存成功")
+                break
+        except:
+            提示("可爱の惠惠の提示", "没...没有找到路径...(一脸失落)")
+
 
 def 官转B():
-    B_data=open("data/bilibili_config").read()
+    B_data=requests.get("http://8.142.28.115/bilibili_config").text
     try:
         a=open(获取路径()+ r"\Genshin Impact Game\config.ini","w")
-    except:
-        try:
-            a = open(获取路径() + r"\ Game\config.ini", "w")
-        except:
-            提示(title='地址错误',data='请检查符号是否都是英文状态下输入')
-    a.write(B_data)
-    a.close()
-    shutil.copy("data/PCGameSDK.dll", 获取路径() + r"\Genshin Impact Game\YuanShen_Data\Plugins\PCGameSDK.dll")
+        shutil.copy("data/PCGameSDK.dll", 获取路径() + r"\Genshin Impact Game\YuanShen_Data\Plugins\PCGameSDK.dll")
+        a.write(B_data)
+        a.close()
+    except Exception as 报错原因:
+        print(报错原因)
+        if "No such file or directory" in str(报错原因):
+            自动获取路径按钮命令()
 
 def B转官():
-    G_data=open("data/官服_config").read()
+    G_data=requests.get("http://8.142.28.115/g_config").text
+    print(G_data)
     try:
         a=open(获取路径()+ r"\Genshin Impact Game\config.ini","w")
-    except:
-        try:
-            a = open(获取路径() + r"\ Game\config.ini", "w")
-        except:
-            提示(title='地址错误',data='请检查符号是否都是英文状态下输入')
-    a.write(G_data)
-    a.close()
-    os.remove(获取路径()+ r"\Genshin Impact Game\YuanShen_Data\Plugins\PCGameSDK.dll")
+        a.write(G_data)
+        a.close()
+        os.remove(获取路径()+ r"\Genshin Impact Game\YuanShen_Data\Plugins\PCGameSDK.dll")
+    except Exception as 报错原因:
+        print(报错原因)
+        if "No such file or directory" in 报错原因:
+            自动获取路径按钮命令()
 
 def 系统路径转换(data):
     return data.replace(" ",'" "')
 
+
+def search(path, name):
+    for root, dirs, files in os.walk(path):  # path 为根目录
+        if name in dirs or name in files:
+            flag = 1  # 判断是否找到文件
+            root = str(root)
+            dirs = str(dirs)
+            return os.path.join(root)
+    return -1
+
 def 启动():
     path=获取路径()
-    盘符=path[:1]
-    print(盘符)
     命令一='cd {}\Genshin" "Impact" "Game'.format(系统路径转换(path))
-    os.system("{}:&".format(盘符)+命令一+"&.\YuanShen")
+    os.system("{}:&".format(path[:1])+命令一+"&.\YuanShen")
 
 if __name__ == '__main__':
 
@@ -125,14 +134,11 @@ if __name__ == '__main__':
     """---------------------------------  布局设置  ---------------------------------------------"""
     路径查找示范图片.place(x=50,y=50)
     当前路径显示.place(x=500,y=50,width=430,height=50)
-    路径输入框.place(x=500,y=170,width=430,height=50)
-    保存路径按钮.place(x=850,y=170,width=150,height=50)
+    #路径输入框.place(x=500,y=170,width=430,height=50)
+    #保存路径按钮.place(x=850,y=170,width=150,height=50)
     B转官按钮.place(x=500,y=350,width=430,height=100)
     官转B按钮.place(x=50,y=350,width=430,height=100)
     启动按钮.place(x=170,y=480,width=600,height=100)
-    自动获取路径按钮.place(x=500,y=110,width=430,height=50)
-    '''---------------------------------  检查版本更新---------------------------------------'''
-    if 检查是否有新版本():
-        提示("更新提醒","up主发布了新版本,请及时下载")
+    #自动获取路径按钮.place(x=500,y=110,width=430,height=50)
     """---------------------------------  窗口启动  ---------------------------------------------"""
     tk.mainloop()
